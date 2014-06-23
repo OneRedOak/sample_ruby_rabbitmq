12b14f5cfc7242719dbd90081d0f2f5a8862e071
Patrick Ellis <oneredoak@gmail.com>
2014-06-23 09:16:41 -0700

    Cleaning Code

--- bowling.rb
+++ bowling.rb
@@ -6,10 +6,8 @@ class Bowling
   def hit()
     conn = Bunny.new(:automatically_recover => false)
     conn.start
-
     ch   = conn.create_channel
     q    = ch.queue("hello")
-
     begin
       ch.default_exchange.publish("Hello World!", :routing_key => q.name)
       consumer = q.subscribe(:block => true) do |delivery_info, properties, body|
